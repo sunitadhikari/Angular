86972675d5f6711e73df6ed843bab25d066e697e
sunitadhikari <sunitaadhikari2001@gmail.com>
2023-06-19 11:47:23 +0545

    lerned yesterdaay which is not in my mind now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { RxjsLearningComponent } from './rxjs-learning/rxjs-learning.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, RxjsLearningComponent],
   imports: [BrowserModule, AppRoutingModule, MatSidenavModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/rxjs-learning/rxjs-learning.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-rxjs-learning',
+  templateUrl: './rxjs-learning.component.html',
+  styleUrls: ['./rxjs-learning.component.css'],
+})
+export class RxjsLearningComponent implements OnInit {
+  agents: Observable<string> | undefined;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
